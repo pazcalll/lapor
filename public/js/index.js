9335c44a1de68b14a16afe433d4685c656d7641b
pazcalll <yazeedarifin@gmail.com>
2022-11-24 13:51:51 +0700

    logout from system

--- public/js/index.js
+++ public/js/index.js
@@ -129,3 +129,19 @@ function homePage() {
         }
     })
 }
+
+function logout() {
+    $.ajax({
+        url: apiBaseUrl + "/user/logout",
+        type: "POST",
+        headers: {
+            Authorization: "bearer " + localStorage.getItem('_token')
+        },
+        success: (res) => {
+            window.location.href = webBaseUrl + "/login"
+        },
+        error: (err) => {
+            console.log(err)
+        }
+    })
+}
