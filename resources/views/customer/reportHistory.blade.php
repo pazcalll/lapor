8848f448738f377cf221d2520a5a03f6dd79c3c5
pazcalll <yazeedarifin@gmail.com>
2022-12-17 15:06:16 +0700

    report history update

--- resources/views/customer/reportHistory.blade.php
+++ resources/views/customer/reportHistory.blade.php
@@ -1,8 +1,7 @@
-<div class="container-fluid hero-header bg-light py-5 mb-5">
+<div class="container-fluid page-heading py-5 mb-5">
     <div class="container py-5">
         <div class="row g-5 align-items-center">
             <div class="col-lg-12">
-                <h1 class="display-4 mb-3">Riwayat Pelaporan</h1>
                 <table class="table table-striped table-borderless" style="width: 100%">
                     <thead>
                         <tr>
@@ -18,3 +17,6 @@
         </div>
     </div>
 </div>
+<script>
+    $('#title-label').html("Riwayat Laporan")
+</script>
